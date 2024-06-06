c998e7feab739e890b3a399b0c02ff6efe53159c
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-06-07 02:40:56 +0700

    refactor code and add generic method

--- src/main/java/com/rainhard/prac/service/HttpClientService.java
+++ src/main/java/com/rainhard/prac/service/HttpClientService.java
@@ -11,10 +11,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Optional;
 
 public class HttpClientService {
 
     static final String url = "https://jsonplaceholder.typicode.com/users";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final HttpClient client = HttpClientFactory.getHttpClient();
+
+
 
     public void getAllUsers() throws URISyntaxException {
 
@@ -45,6 +50,69 @@ public class HttpClientService {
         }
     }
 
+    public void getUserById() throws URISyntaxException {
+
+        try{
+
+            HttpClient client = HttpClientFactory.getHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(url))
+                    .GET().build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); //Read json as String
+
+            if(response.statusCode() == 200) {
+
+                String responseBody = response.body();
+                List<User> users = objectMapper().readValue(responseBody, new TypeReference<List<User>>() {});
+                users.stream()
+                        .filter(user -> user.getId() == 1)
+                        .toList()
+                        .forEach(System.out::println);
+
+            }else if (response.statusCode() == 404) {
+                System.out.println("Not Found. status code: " + response.statusCode());
+            }else{
+                System.out.println("There's an error");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    public List<User> findAllUser() throws URISyntaxException {
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI(url)).build();
+        return request(request, new TypeReference<List<User>>() {
+        });
+    }
+
+    public Optional<User> findUserByName(String name) throws URISyntaxException {
+        return findAllUser().stream()
+                .filter(userName -> userName.getName().equals(name))
+               .findFirst();
+    }
+
+    private <T> T request(HttpRequest httpRequest, TypeReference<T> tTypeReference) throws URISyntaxException{
+        try{
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 200) {
+                String body = response.body();
+                return objectMapper().readValue(body, tTypeReference);
+            }else if (response.statusCode() == 404) {
+                System.out.println("Not Found. status code: " + response.statusCode());
+            }else{
+                System.out.println("There's an error");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+        return  null;
+    }
 
     private ObjectMapper objectMapper(){
         return new ObjectMapper();
